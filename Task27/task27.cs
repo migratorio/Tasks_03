c44759c5fdb923039ce3ef7a4d8a79b2c546c892
migratorio <migratorio@yandex.ru>
2022-10-15 20:40:28 +0300

    Программа принимает на вход число и выдаёт сумму цифр в числе

--- Task27/task27.cs
+++ Task27/task27.cs
@@ -1,7 +1,38 @@
-﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿/*Программа принимает на вход число и выдаёт сумму цифр в числе.
 452 -> 11
 82 -> 10
 9012 -> 12
 */
 
 Console.Clear();
+
+int N = EnterNumber("Введите целое число N: ", "Ошибка ввода");
+SummOfNumber(N);
+	
+//-----------------------------------
+//Функция позволяет ввести исходные данные с клавиатуры
+static int EnterNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int N);
+        if (isCorrect)
+            return N;
+        Console.WriteLine(errorMessage);
+    }
+}
+//Функция подсчитывает сумму цифр переданного числа
+static void SummOfNumber(int N)
+{
+	int sum = 0;
+	int tempN = Math.Abs(N);
+	for(; tempN >= 1;)		// Считаем сумму цифр
+	{
+		int modulo = tempN % 10;
+		tempN = tempN/10;
+		sum += modulo;
+	}
+	Console.WriteLine($"{N} -> {sum}");
+}
+//----------------------------------
